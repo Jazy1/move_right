bc77d646afe3feadc7f23c6969857dd086b28d6b
Muhammad Arslan Shafique <133588853+Arsuuu@users.noreply.github.com>
2024-01-29 15:51:09 +0500

    Arsu comit

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
+//hello
 Route::get("/", function(){
     return view("home");
 })->name("public.home");
